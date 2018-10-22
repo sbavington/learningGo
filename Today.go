f85297e20a8b8d1834c2b2dab54950fb4487d677
Stephen Bavington <sbavington@mac.com>
2018-10-22 12:52:37 -0400

    First test commit

---
+++ Today.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("When's Wednesday?")
+	today := time.Now().Weekday()
+	fmt.Println(today)
+	switch time.Wednesday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+}
